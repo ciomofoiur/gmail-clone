ae8ee315bcec9c3eccdc1d581adad253cbede6a0
raoulciomofoiu <raoul.ciomofoiu@pixelsolutions.ro>
2022-06-23 16:44:42 +0300

    done

---
+++ src/components/MailView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="email-display">
+    <div>
+      <button @click="toggleArchive">
+        {{ email.archived ? "Move to Inbox (E)" : "Archive (E)" }}
+      </button>
+      <button @click="toggleRead">
+        {{ email.read ? "Mark Unread (R)" : "Mark Read (R)" }}
+      </button>
+      <button @click="goNewer">Newer (K)</button>
+      <button @click="goOlder">Older (J)</button>
+    </div>
+    <h2 class="mb-0">
+      Subject: <strong>{{ email.subject }}</strong>
+    </h2>
+    <div>
+      <em>From {{ email.from }} on {{ format(new Date(email.sentAt), "MMM do yyyy") }}</em>
+    </div>
+    <div v-html="marked(email.body)" />
+  </div>
+</template>
+
+<script>
+import { format } from "date-fns";
+import marked from "marked";
+import useKeydown from "../composables/use-keydown";
+
+export default {
+  setup(props, { emit }) {
+    let toggleRead = () => emit("changeEmail", { toggleRead: true, save: true });
+    let toggleArchive = () =>
+      emit("changeEmail", { toggleArchive: true, save: true, closeModal: true });
+    let goNewer = () => emit("changeEmail", { changeIndex: -1 });
+    let goOlder = () => emit("changeEmail", { changeIndex: 1 });
+    let goNewerAndArchive = () =>
+      emit("changeEmail", { changeIndex: -1, toggleArchive: true, save: true });
+    let goOlderAndArchive = () =>
+      emit("changeEmail", { changeIndex: 1, toggleArchive: true, save: true });
+
+    useKeydown([
+      { key: "r", fn: toggleRead },
+      { key: "e", fn: toggleArchive },
+      { key: "k", fn: goNewer },
+      { key: "j", fn: goOlder },
+      { key: "[", fn: goNewerAndArchive },
+      { key: "]", fn: goOlderAndArchive }
+    ]);
+
+    return {
+      format,
+      marked,
+      toggleRead,
+      toggleArchive,
+      goNewer,
+      goOlder
+    };
+  },
+  props: {
+    email: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
